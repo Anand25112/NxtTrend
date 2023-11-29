749c6a50417c5799d296ee5abdec64b3d5e39a67
githubuserarun <arunkumar435771@gmail.com>
2023-11-29 18:08:18 +0000

    update cart summary

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,8 +1,5 @@
 import Popup from 'reactjs-popup'
-
 import CartContext from '../../context/CartContext'
-import CheckOutPopup from '../CheckoutPopup'
-
 import './index.css'
 
 const CartSummary = () => (
@@ -14,6 +11,19 @@ const CartSummary = () => (
         total += eachCartItem.price * eachCartItem.quantity
       })
 
+      let a = true
+
+      const onChangeSelect = event => {
+        console.log(event.target.value)
+        if (String(event.target.value) === 'Cash on Delivery') {
+          a = false
+        } else {
+          a = true
+        }
+      }
+
+      console.log(a)
+
       return (
         <>
           <div className="cart-summary-container">
@@ -23,33 +33,59 @@ const CartSummary = () => (
             </h1>
             <p className="total-items">{cartList.length} Items in cart</p>
 
-            <div>
-              <Popup
-                trigger={
-                  <button
-                    type="button"
-                    className="checkout-button d-sm-none w-50"
-                  >
-                    Checkout
+            <Popup
+              trigger={
+                <button type="button" className="checkout-button button">
+                  Checkout
+                </button>
+              }
+              modal
+              nested
+            >
+              {close => (
+                <div className="modal">
+                  <button type="button" className="close" onClick={close}>
+                    &times;
                   </button>
-                }
-                position="top right"
-              >
-                <CheckOutPopup total={total} item={cartList} />
-              </Popup>
-            </div>
-            <div>
-              <Popup
-                trigger={
-                  <button type="button" className="checkout-button d-lg-none">
-                    Checkout
-                  </button>
-                }
-                position="top center"
-              >
-                <CheckOutPopup total={total} item={cartList} />
-              </Popup>
-            </div>
+                  <div className="header"> Payment </div>
+                  <div className="content">
+                    <h1>Payment options</h1>
+                    <p>Total Items: {cartList.length}</p>
+                    <p>Total Price: {total} Rs</p>
+                    <form onChange={onChangeSelect}>
+                      <input type="radio" disabled="true" />
+                      Net Banking
+                      <input type="radio" />
+                      Cash on Delivery
+                    </form>
+                  </div>
+                  <div className="actions">
+                    <Popup
+                      trigger={
+                        <button type="button" className="button">
+                          Confirm Order
+                        </button>
+                      }
+                      position="top center"
+                      nested
+                    >
+                      <div className="successMsg">
+                        <p>Your order has been placed successfully</p>
+                      </div>
+                    </Popup>
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={() => {
+                        close()
+                      }}
+                    >
+                      Cancel Payment
+                    </button>
+                  </div>
+                </div>
+              )}
+            </Popup>
           </div>
         </>
       )
